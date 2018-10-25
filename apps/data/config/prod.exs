dd8484eb1ca98c0dbd0368dd0a3d448783d443bc
Johnny Winn <j.winn.v@gmail.com>
2018-10-25 07:24:40 -0600

    Deployment changes

---
+++ apps/data/config/prod.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+config :data,
+  Data.ReadOnly.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+config :data,
+  Data.WriteOnly.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  migration_primary_key: [id: :uuid, type: :binary_id],
+  migration_timestamps: [type: :utc_datetime],
+  ssl: true
