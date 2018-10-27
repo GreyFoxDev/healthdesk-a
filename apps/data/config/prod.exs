73cf0466eb501a62410c65edd4d81660fc2bce31
Johnny Winn <j.winn.v@gmail.com>
2018-10-27 17:21:13 -0600

    Update readonly repo config

--- apps/data/config/prod.exs
+++ apps/data/config/prod.exs
@@ -4,6 +4,8 @@ config :data,
   Data.ReadOnly.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  migration_primary_key: [id: :uuid, type: :binary_id],
+  migration_timestamps: [type: :utc_datetime],
   ssl: true
 
 config :data,
