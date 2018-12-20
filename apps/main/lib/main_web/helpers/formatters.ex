219b9e18fee4c3ffe3df515b9e4ed3f38ebbbb6b
Andrew Phillipo <andrewphillipo@gmail.com>
2018-12-20 01:01:38 +0000

    finally fix formatting of date strings

--- apps/main/lib/main_web/helpers/formatters.ex
+++ apps/main/lib/main_web/helpers/formatters.ex
@@ -1,5 +1,5 @@
 defmodule MainWeb.Helper.Formatters  do
-
+  alias Calendar.Strftime
   require Logger
 
   def format_phone(<< "+1", area_code::binary-size(3), prefix::binary-size(3), line::binary-size(4) >>) do
@@ -26,23 +26,20 @@ defmodule MainWeb.Helper.Formatters  do
   def parse_date({:ok, seconds, _, _}, _date) when seconds < 7200, do: "1 hour ago"
   def parse_date({:ok, seconds, _, _}, _date) when seconds < 18000, do: "#{div(seconds, 3600)} hours ago"
   def parse_date({:ok, seconds, _, _}, datetime) when seconds < 86400 do
-
     datetime
     |> Calendar.DateTime.to_time()
     |> Calendar.Time.Format.iso8601()
     |> to_time(:today)
-
   end
   def parse_date({:ok, seconds, _, _}, datetime) when seconds < 172800 do
-
     datetime
     |> Calendar.DateTime.to_time()
     |> Calendar.Time.Format.iso8601()
     |> to_time(:yesterday)
-
   end
   def parse_date(_seconds, date) do
-    Calendar.DateTime.Format.httpdate(date)
+    Strftime.strftime!(date, "%d/%m/%Y")
+    #Calendar.DateTime.Format.httpdate(date)
   end
 
   def to_time(<< hour::binary-size(2), ":", minute::binary-size(2), _rest::binary >>, :today) do
