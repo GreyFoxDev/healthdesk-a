291d2b85f7e09e8249ac947abdd5515630af0f9c
Johnny Winn <j.winn.v@gmail.com>
2019-02-02 14:47:06 -0700

    Set the database name

--- apps/data/config/dev.exs
+++ apps/data/config/dev.exs
@@ -2,16 +2,12 @@ use Mix.Config
 
 config :data,
        Data.ReadOnly.Repo,
-       username: "postgres",
-       password: "postgres",
-       database: "d3rdui5c53hgjm",
+       database: "healthdesk_dev",
        migration_primary_key: [id: :uuid, type: :binary_id],
        migration_timestamps: [type: :utc_datetime]
 
 config :data,
        Data.WriteOnly.Repo,
-       username: "postgres",
-       password: "postgres",
-       database: "d3rdui5c53hgjm",
+       database: "healthdesk_dev",
        migration_primary_key: [id: :uuid, type: :binary_id],
        migration_timestamps: [type: :utc_datetime]
