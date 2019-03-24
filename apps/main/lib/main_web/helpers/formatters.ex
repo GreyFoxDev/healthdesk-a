9d53a0165478f539545758661b936ff6f85f7c54
Johnny Winn <j.winn.v@gmail.com>
2019-03-24 13:23:40 -0600

    Fix dates again

--- apps/main/lib/main_web/helpers/formatters.ex
+++ apps/main/lib/main_web/helpers/formatters.ex
@@ -28,37 +28,41 @@ defmodule MainWeb.Helper.Formatters  do
     datetime = Calendar.DateTime.shift_zone!(datetime, timezone)
     now = Calendar.DateTime.shift_zone!(DateTime.utc_now(), timezone)
 
-    now
-    |> Calendar.DateTime.diff(datetime)
-    |> parse_date(datetime)
+    date = DateTime.to_date(datetime)
+    now_date = DateTime.to_date(now)
+
+    case Date.diff(date, now_date) do
+      0 -> # Today
+        now
+        |> Calendar.DateTime.diff(datetime)
+        |> parse_date(datetime)
+      -1 -> # Yesterday
+        datetime
+        |> Calendar.DateTime.to_time()
+        |> Calendar.Time.Format.iso8601()
+        |> to_time(:yesterday)
+      _ ->
+        Strftime.strftime!(datetime, "%m/%d/%Y")
+    end
   end
 
-  def parse_date({:ok, seconds, _, _}, _date) when seconds < 60, do: "now"
-  def parse_date({:ok, seconds, _, _}, _date) when seconds < 120, do: "1 minute ago"
-  def parse_date({:ok, seconds, _, _}, _date) when seconds < 3600, do: "#{div(seconds, 60)} minutes ago"
-  def parse_date({:ok, seconds, _, _}, _date) when seconds < 7200, do: "1 hour ago"
-  def parse_date({:ok, seconds, _, _}, _date) when seconds < 18000, do: "#{div(seconds, 3600)} hours ago"
-  def parse_date({:ok, seconds, _, _}, datetime) when seconds < 86400 do
-    datetime
+  defp parse_date({:ok, seconds, _, _}, _date) when seconds < 60, do: "now"
+  defp parse_date({:ok, seconds, _, _}, _date) when seconds < 120, do: "1 minute ago"
+  defp parse_date({:ok, seconds, _, _}, _date) when seconds < 3600, do: "#{div(seconds, 60)} minutes ago"
+  defp parse_date({:ok, seconds, _, _}, _date) when seconds < 7200, do: "1 hour ago"
+  defp parse_date({:ok, seconds, _, _}, _date) when seconds < 18000, do: "#{div(seconds, 3600)} hours ago"
+  defp parse_date({:ok, seconds, _, _}, datetime) do
+    time = datetime
     |> Calendar.DateTime.to_time()
     |> Calendar.Time.Format.iso8601()
     |> to_time(:today)
   end
-  def parse_date({:ok, seconds, _, _}, datetime) when seconds < 172800 do
-    datetime
-    |> Calendar.DateTime.to_time()
-    |> Calendar.Time.Format.iso8601()
-    |> to_time(:yesterday)
-  end
-  def parse_date(_seconds, datetime) do
-    Strftime.strftime!(datetime, "%m/%d/%Y")
-  end
 
-  def to_time(<< hour::binary-size(2), ":", minute::binary-size(2), _rest::binary >>, :today) do
+  defp to_time(<< hour::binary-size(2), ":", minute::binary-size(2), _rest::binary >>, :today) do
     "Today at #{adjust_hour(hour, minute)}"
   end
 
-  def to_time(<< hour::binary-size(2), ":", minute::binary-size(2), _rest::binary >>, :yesterday) do
+  defp to_time(<< hour::binary-size(2), ":", minute::binary-size(2), _rest::binary >>, :yesterday) do
     "Yesterday at #{adjust_hour(hour, minute)}"
   end
 
