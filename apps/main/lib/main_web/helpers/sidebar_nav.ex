549ce1777121e54a521a66028e13aab07726a1bd
Johnny Winn <j.winn.v@gmail.com>
2019-04-06 10:38:00 -0600

    Only show assigned and open conversations for teammates

--- apps/main/lib/main_web/helpers/sidebar_nav.ex
+++ apps/main/lib/main_web/helpers/sidebar_nav.ex
@@ -2,11 +2,11 @@ defmodule MainWeb.Helper.SidebarNav do
 
   def get_nav(current_user, location_id \\ nil)
   def get_nav(%{role: "admin"}, _location_id) do
-    
+
   end
 
-  def get_nav(%{role: "team_member"}, _location_id) do
-    
+  def get_nav(%{role: "teammate"}, _location_id) do
+
   end
 
 end
