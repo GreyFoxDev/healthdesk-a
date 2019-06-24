fff9b46de11d116e2a0859b2644c9c93eaa10d89
Johnny Winn <j.winn.v@gmail.com>
2019-06-24 08:11:57 -0700

    Team dropdown for metrics

--- apps/main/lib/main_web/controllers/admin_controller.ex
+++ apps/main/lib/main_web/controllers/admin_controller.ex
@@ -4,7 +4,7 @@ defmodule MainWeb.AdminController do
   alias Data.Metrics
 
   def index(conn, %{"team_id" => team_id}) do
-    render(conn, "index.html", metrics: [Metrics.team(team_id)], teams: teams(conn))
+    render(conn, "index.html", metrics: [Metrics.team(team_id)], teams: teams(conn), team_id: team_id, location: nil)
   end
 
   def index(conn, _params) do
@@ -12,9 +12,9 @@ defmodule MainWeb.AdminController do
     teams = teams(conn)
 
     if current_user.role == "admin" do
-      render(conn, "index.html", metrics: Metrics.all_teams(), teams: teams, location: nil)
+      render(conn, "index.html", metrics: Metrics.all_teams(), teams: teams, location: nil, team_id: nil)
     else
-      render(conn, "index.html", metrics: [Metrics.team(current_user.team_member.team_id)], teams: teams, location: nil)
+      render(conn, "index.html", metrics: [Metrics.team(current_user.team_member.team_id)], teams: teams, location: nil, team_id: nil)
     end
   end
 
