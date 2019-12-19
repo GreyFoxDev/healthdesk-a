8af3e50b7f706a44c409c5f16fd5274b3455c2d5
Johnny Winn <j.winn.v@gmail.com>
2019-12-19 10:15:21 -0800

    Facebook messenger params

--- apps/main/lib/main_web/plugs/assign_params.ex
+++ apps/main/lib/main_web/plugs/assign_params.ex
@@ -16,14 +16,22 @@ defmodule MainWeb.Plug.AssignParams do
   makes it easier to access in the pipeline.
   """
   @spec call(Plug.Conn.t(), list()) :: Plug.Conn.t()
-  def call(%{params: %{"Body" => body, "From" => member, "To" => location}} = conn, _opts) do
-    conn
-    |> assign(:message, body)
-    |> assign(:member, member)
-    |> assign(:location, location)
+  def call(%{params: %{"Body" => body, "From" => member, "To" => location} = params} = conn, _opts) do
+    if String.starts_with?(location, "messenger:") do
+      conn
+      |> assign(:message, body)
+      |> assign(:member, member)
+      |> assign(:location, "+13105893019")
+    else
+      conn
+      |> assign(:message, body)
+      |> assign(:member, member)
+      |> assign(:location, location)
+    end
   end
 
   def call(%{params: %{"flow_name" => flow} = params} = conn, _opts) do
+
     location = Location.get(%{role: "admin"}, params["location_id"])
 
     conn
