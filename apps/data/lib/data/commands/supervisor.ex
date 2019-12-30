dc8be374c6cff687d326bc30fe428d2b6d69ea9f
Johnny Winn <j.winn.v@gmail.com>
2019-12-30 07:54:14 -0800

    Extend wait time for queries

--- apps/data/lib/data/commands/supervisor.ex
+++ apps/data/lib/data/commands/supervisor.ex
@@ -7,7 +7,7 @@ defmodule Data.Commands.Supervisor do
 
   def init(_arg) do
     children = [
-      {Task.Supervisor, name: Data.Commands}
+      {Task.Supervisor, name: Data.Commands, restart: :transient}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
@@ -16,7 +16,7 @@ defmodule Data.Commands.Supervisor do
   def execute_task_with_results(func) do
     Data.Commands
     |> Task.Supervisor.async(func)
-    |> Task.await()
+    |> Task.await(10_000)
   end
 
   def execute_task(func) do
