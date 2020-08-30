cd01ec10c22ef629b872de67ea1f1c2589fcb58e
Hammad <hammad@tanbits.com>
2020-08-30 11:40:36 +0500

    format median time

--- apps/main/lib/main_web/helpers/formatters.ex
+++ apps/main/lib/main_web/helpers/formatters.ex
@@ -144,13 +144,13 @@ defmodule MainWeb.Helper.Formatters  do
   end
 
   defp sec_string(s) when s ==0, do: "N/A"
-  defp sec_string(s) when s < 10, do: "0#{s} secs"
-  defp sec_string(s), do: "#{s} secs"
+  defp sec_string(s) when s < 10, do: "0#{s}s"
+  defp sec_string(s), do: "#{s}s"
   defp min_string(s) when s == 0, do: ""
-  defp min_string(s) when s < 10, do: "0#{s} mins, "
-  defp min_string(s), do: "#{s} mins, "
+  defp min_string(s) when s < 10, do: "0#{s}m "
+  defp min_string(s), do: "#{s}m "
   defp hour_string(s) when s == 0, do: ""
-  defp hour_string(s) when s < 10, do: "0#{s} hrs, "
-  defp hour_string(s), do: "#{s} hrs, "
+  defp hour_string(s) when s < 10, do: "0#{s}h "
+  defp hour_string(s), do: "#{s}h "
 
 end
