245cc8307d7579fca87d20da1306d4149f81cbc4
Hammad <hammad@tanbits.com>
2020-10-04 19:07:34 +0500

    fix

--- apps/main/lib/main_web/views/layout_view.ex
+++ apps/main/lib/main_web/views/layout_view.ex
@@ -15,7 +15,17 @@ defmodule MainWeb.LayoutView do
     MainWeb.Auth.Guardian.Plug.current_resource(conn)
   end
 
+  def teammate_locations(%Data.Schema.User{}=current_user) do
+
+    current_user.team_member.team_member_locations
+    |> Stream.map(&(&1.location))
+    |> Stream.filter(&(&1.deleted_at == nil))
+    |> Enum.to_list()
+    |> Kernel.++([current_user.team_member.location])
+    |> Enum.dedup_by(&(&1.id))
+  end
   def teammate_locations(conn) do
+
     current_user = current_user(conn)
 
     current_user.team_member.team_member_locations
