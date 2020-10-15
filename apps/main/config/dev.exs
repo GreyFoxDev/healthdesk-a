5c85a250ad49daa1947bfe26e9882a78dda06185
Hammad <hammad@tanbits.com>
2020-10-15 22:26:48 +0500

    watchers

--- apps/main/config/dev.exs
+++ apps/main/config/dev.exs
@@ -4,9 +4,7 @@ config :main, MainWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  check_origin: false
 
 config :main, Main.Mailer,
   adapter: Bamboo.LocalAdapter
