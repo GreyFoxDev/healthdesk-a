dc83c8290c2ab3ee6e363317c57dab9ef61a0e87
Hammad <hammad@tanbits.com>
2021-04-07 17:41:00 +0500

    add email to member

--- apps/main/lib/main_web/plugs/save_member_data.ex
+++ apps/main/lib/main_web/plugs/save_member_data.ex
@@ -10,6 +10,28 @@ defmodule MainWeb.Plug.SaveMemberData do
   @spec init(list()) :: list()
   def init(opts), do: opts
 
+  def call(%{assigns: %{memberName: name, phoneNumber: phone, location: location, email: email} = assigns} = conn, _opts) when phone != nil do
+    l = Location.get_by_phone(location)
+    phone = format_phone(phone)
+    [first_name, last_name] =
+      name
+      |> String.split(" ")
+      |> format_name()
+
+    {:ok, member} =
+      with %Data.Schema.Member{} = member <- Member.get_by_phone_number(@role, phone) do
+        update_member_data(member.id, first_name, last_name, email)
+      else
+        nil ->
+          create_member_data(l.team_id, first_name, last_name, phone, email)
+      end
+
+    if MemberChannel.get_by_channel_id(assigns.member) == [] do
+      MemberChannel.create(%{member_id: member.id, channel_id: assigns.member})
+    end
+
+    conn
+  end
   def call(%{assigns: %{memberName: name, phoneNumber: phone, location: location} = assigns} = conn, _opts) when phone != nil do
     l = Location.get_by_phone(location)
     phone = format_phone(phone)
@@ -48,6 +70,22 @@ defmodule MainWeb.Plug.SaveMemberData do
 
   def call(conn, _opts), do: conn
 
+  defp create_member_data(team_id, first_name, nil, phone,email) do
+    Member.create(%{
+          team_id: team_id,
+          first_name: first_name,
+          email: email,
+          phone_number: phone})
+  end
+
+  defp create_member_data(team_id, first_name, last_name, phone,email) do
+    Member.create(%{
+          team_id: team_id,
+          first_name: first_name,
+          email: email,
+          last_name: last_name,
+          phone_number: phone})
+  end
   defp create_member_data(team_id, first_name, nil, phone) do
     Member.create(%{
           team_id: team_id,
@@ -63,7 +101,13 @@ defmodule MainWeb.Plug.SaveMemberData do
           phone_number: phone})
   end
 
-  defp update_member_data(member_id, first_name, nil) do
+  defp update_member_data(member_id, first_name, nil, email) do
+    Member.update(member_id, %{first_name: first_name, email: email})
+  end
+  defp update_member_data(member_id, first_name, last_name , email) do
+    Member.update(member_id, %{first_name: first_name, last_name: last_name, email: email})
+  end
+    defp update_member_data(member_id, first_name, nil) do
     Member.update(member_id, %{first_name: first_name})
   end
 
