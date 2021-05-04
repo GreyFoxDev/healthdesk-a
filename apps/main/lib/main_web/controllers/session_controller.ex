4a2a10f67c912a2c27e5089976ec47b508759cd4
Hammad <hammad@tanbits.com>
2021-05-04 19:12:56 +0500

    Revert "fixes"
    This reverts commit b82376f2

--- apps/main/lib/main_web/controllers/session_controller.ex
+++ apps/main/lib/main_web/controllers/session_controller.ex
@@ -25,8 +25,8 @@ defmodule MainWeb.SessionController do
   end
 
   def create(conn, %{"session" => %{"verification_code" => code, "phone_number" => phone_number}}) do
-    with user when not is_nil(user) <- Query.get_by_phone(phone_number)
-          do
+    with user when not is_nil(user) <- Query.get_by_phone(phone_number),
+         :ok <- Twilio.check(phone_number, code) do
             Query.update(user.id, %{logged_in_at: DateTime.utc_now()})
       case user.role do
         "admin" ->
@@ -47,8 +47,9 @@ defmodule MainWeb.SessionController do
   end
 
   def create(conn, %{"session" => %{"phone_number" => phone_number}}) do
-    with user when not is_nil(user) <- Query.get_by_phone(phone_number)
-      do
+    with user when not is_nil(user) <- Query.get_by_phone(phone_number),
+         :ok <- Twilio.verify(phone_number) do
+
       conn
       |> put_layout(:login)
       |> put_flash(:success, "Please verify the phone number #{user.first_name}!")
