c53bb31aa55bf55d384ec0c118c7084974814571
Tayyab Butt <wasibiit@gmail.com>
2021-08-17 17:29:46 +0500

    uncomment session

--- apps/main/lib/main_web/controllers/session_controller.ex
+++ apps/main/lib/main_web/controllers/session_controller.ex
@@ -27,8 +27,8 @@ defmodule MainWeb.SessionController do
   def create(conn, %{"session" => %{"verification_code" => code, "phone_number" => phone_number}}) do
 
     with user when not is_nil(user) <- Query.get_by_phone(phone_number) do
-#      with user when not is_nil(user) <- Query.get_by_phone(phone_number),
-#         :ok <- Twilio.check(phone_number,user.country, code)do
+      with user when not is_nil(user) <- Query.get_by_phone(phone_number),
+        :ok <- Twilio.check(phone_number,user.country, code)do
       Query.update(user.id, %{logged_in_at: DateTime.utc_now()})
       case user.role do
         "admin" ->
@@ -51,8 +51,8 @@ defmodule MainWeb.SessionController do
   def create(conn, %{"session" => %{"phone_number" => phone_number}}) do
 
     with user when not is_nil(user) <- Query.get_by_phone(phone_number)do
-#      with user when not is_nil(user) <- Query.get_by_phone(phone_number),
-#         :ok <- Twilio.verify(phone_number,user.country)do
+      with user when not is_nil(user) <- Query.get_by_phone(phone_number),
+         :ok <- Twilio.verify(phone_number,user.country)do
       conn
       |> put_layout(:login)
       |> put_flash(:success, "Please verify the phone number #{user.first_name}!")
